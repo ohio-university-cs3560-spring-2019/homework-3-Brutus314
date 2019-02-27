8bd50650b938316068ab74247ba4ff634513c2d9
Alex Engle <ae615516@ohio.edu>
2019-02-27 16:41:00 -0500

    First Submission of assignment 7

---
+++ hw7/hw7.cc
@@ -0,0 +1,29 @@
+#include "hw7.h"
+#include <iostream>
+#include <cmath>
+
+double stddev( int n, ... )
+{
+	int sum = 0;
+	va_list args;
+	va_start(args, n);
+	for(int i = 0; i < n; i++)
+	{
+		int num = va_arg(args, int);
+		sum += num;
+	}
+	va_end(args);
+	va_start(args, n);
+	double mean = static_cast<double>(sum) / n;
+	double stddev = 0;
+	for(int i = 0; i < n; i++)
+	{
+		int num = va_arg(args, int);
+		stddev = stddev + (num - mean) * (num - mean);
+	}
+	va_end(args);
+	stddev /= n;
+	if( stddev == 0)
+		std::cout << "Sigma is zero." << std::endl;
+	return sqrt(stddev);
+}
